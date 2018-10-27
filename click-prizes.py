fed5cf1ac95d45ec91ceec3635f5ee271b1d8663
zongjie-mao <mao_zongjie@sina.com>
2018-10-27 22:14:24 +0800

    recursive to click_prize

--- click-prizes.py
+++ click-prizes.py
@@ -7,9 +7,26 @@ cordination_dic = {'ten_item':(648,636),'reset':(1641,370),
     'confirm':(1215,823),'close':(941,818)}
 
 def time_print(content_str):
-    return '%s: %s'%(time.asctime(),content_str)
+    print('%s: %s'%(time.asctime(),content_str))
 
-def click_price(times=130):
+def get_num(num_str,**kw):
+    out_num = float(input('please enter %s:'%(num_str)))
+    if 'max_num' in kw.keys():
+        max_entered = kw['max_num']
+        if out_num <= max_entered:
+            pass
+        else:
+            print('wrong number entered,the number must no more than %s'%(max_entered))
+            get_num(num_str,**kw)
+    if 'min_num' in kw.keys():
+        min_entered = kw['min_num']
+        if out_num >= min_entered:
+            return out_num
+        else:
+            print('wrong number entered,the number must no less than %s'%(min_entered))
+            get_num(num_str,**kw)
+
+def click_prize(times=130):
     for i in range(times):
         pyautogui.click(cordination_dic['ten_item'][0],cordination_dic['ten_item'][1],duration=0.3)
 
@@ -17,42 +34,31 @@ def click_new_pool():
     time.sleep(5)
     for key in ['reset','confirm','close']:
         pyautogui.click(cordination_dic[key][0],cordination_dic[key][1],duration=3)
-    
-def click_all_flower():
-    flower_num = int(input('please enter flower number:'))
-    current_rest = int(input('please enter rest item num in current pool:'))
-    if current_rest > 0 and current_rest != 300:
-        print(time_print('clicking rest item in current pool...'))
-        flower_num = flower_num-current_rest*2
-        if flower_num > 0:
-            current_click_times = math.ceil(current_rest/10)*4+5
-        else:
-            current_click_times = math.ceil((current_rest*2+flower_num)/20)*4+5
-        time.sleep(5)
-        click_price(current_click_times)
-        print(time_print('rest item in current pool click finished...'))
-        if flower_num > 0:
-            click_new_pool()
-        else:
-            pass
-    elif current_rest == 0:
+
+flower_num_input = get_num('the flower\'s total num',**{'min_num':0,'max_num':6000})
+rest_item_input = get_num('the item num that left in current pool',**{'min_num':0,'max_num':300})
+pool_nums = 0
+
+def click_prize_recursive(flower_num,rest_item=300):
+    if rest_item == 0:
         click_new_pool()
+        rest_item = 300
+        click_prize_recursive(flower_num)
+    elif flower_num <= rest_item*2:
+        time_print('click the last pool:')
+        time.sleep(5)
+        click_prize(math.ceil(flower_num*0.2)+5)
+        time_print('finished')
     else:
-        pass
-    time.sleep(5)
-    if flower_num <= 600:
-        print(time_print('start click pool...'))
-        click_price(math.ceil(flower_num/20)*4+5)
-        print(time_print('finished'))
-    else:
-        i = math.ceil(flower_num/600)
-        remain_num = flower_num%600
-        for j in range(i-1):
-            print(time_print('start print num %s pool...'%(str(j+1))))
-            click_price()
-            print(time_print('num %s pool click finished'%(str(j+1))))
-            click_new_pool()
-        click_price(times=math.ceil(remain_num/20)*4+5)
+        global pool_nums 
+        pool_nums += 1
+        time_print('begin to click num %s pool...'%(pool_nums))
+        time.sleep(5)
+        click_prize(math.ceil(rest_item*0.4)+5)
+        time_print('num %s pool finished...'%(pool_nums))
+        flower_num = flower_num-rest_item*2
+        click_new_pool()
+        click_prize_recursive(flower_num)
         
 if __name__ == '__main__':
-    click_all_flower()
+    click_prize_recursive(flower_num_input,rest_item_input)
---
+++ print_as_return.py
@@ -0,0 +1,13 @@
+def ppt(word):
+    print('this dis print str of %s'%(word))
+
+a = 1
+
+def print_a():
+    a += 1
+    print(a)
+
+
+if __name__ == '__main__':
+    print_a()
+    
